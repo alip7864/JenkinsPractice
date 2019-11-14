8e6cc1a70df0d71db5c10bf82f27ce75ca3bcc7b
alip7864 <51531000+alip7864@users.noreply.github.com>
2019-11-14 18:16:16 +0530

    Update Build.bat

--- Build.bat
+++ Build.bat
@@ -1,4 +1,1 @@
-echo ""[DBuilding the project : %date% : %time%;dsfsssafdc[A[A[A[A[A[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[Dcat Build.bat
-
-
 echo " Building the project : %date% : %time%"
